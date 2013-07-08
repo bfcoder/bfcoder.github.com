63fa39bf780a08c8890f667129403124fc066764
James Carbine <JamesCarbine@gmail.com>
2013-07-07 22:31:17 -0600

    updated turtle story

--- turtle/assets/javascripts/turtle.js
+++ turtle/assets/javascripts/turtle.js
@@ -1,33 +1,37 @@
 $(document).ready(function() {
-  var left_position = {
-    north_america: 345,
-    south_america: 508,
-    africa: 975
-  }
-  $.each(left_position, function(key, value){
+  setTimeout(function(){
 
-    $('.'+key).animate(
-    { left: value }, {
-      duration: 1250,
-      easing: 'easeInOutElastic'
+    var left_position = {
+      north_america: 345,
+      south_america: 508,
+      africa: 880
+    }
+    $.each(left_position, function(key, value){
+
+      $('.'+key).animate(
+      { left: value }, {
+        duration: 1250,
+        easing: 'easeInOutElastic'
+      });
+
     });
 
-  });
+    var right_position = {
+      europe: 880,
+      asia: 1240,
+      australia: 1410,
+      antartica: 800
+    }
+    $.each(right_position, function(key, value){
 
-  var right_position = {
-    europe: 240,
-    asia: -140,
-    australia: -400,
-    antartica: 200
-  }
-  $.each(right_position, function(key, value){
+      $('.'+key).animate(
+      { left: value }, {
+        duration: 1250,
+        easing: 'easeInOutElastic'
+      });
 
-    $('.'+key).animate(
-    { right: value }, {
-      duration: 1250,
-      easing: 'easeInOutElastic'
     });
 
-  });
+  },3000);
 
 });
