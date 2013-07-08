20556183875e6d9ee6baaa08e63dc2a64bba40ac
James Carbine <JamesCarbine@gmail.com>
2013-07-07 23:41:56 -0600

    updated turtle story

--- turtle/assets/javascripts/turtle.js
+++ turtle/assets/javascripts/turtle.js
@@ -1,37 +1,27 @@
 $(document).ready(function() {
-  setTimeout(function(){
+  var left_position = {
+    north_america: 345,
+    south_america: 508,
+    africa: 880,
+    europe: 880,
+    asia: 1240,
+    australia: 1410,
+    antartica: 800
+  }
 
-    var left_position = {
-      // north_america: 345,
-      // south_america: 508,
-      africa: 880
-    }
-    $.each(left_position, function(key, value){
+  var index = 0;
+  var duration = 1250;
+  $.each(left_position, function(key, value){
+    index++;
 
+    setTimeout(function(){
       $('.'+key).animate(
       { left: value }, {
-        duration: 1250,
+        duration: duration,
         easing: 'easeInOutElastic'
       });
+    }, duration * index);
 
-    });
-
-    // var right_position = {
-    //   europe: 880,
-    //   asia: 1240,
-    //   australia: 1410,
-    //   antartica: 800
-    // }
-    // $.each(right_position, function(key, value){
-
-    //   $('.'+key).animate(
-    //   { left: value }, {
-    //     duration: 1250,
-    //     easing: 'easeInOutElastic'
-    //   });
-
-    // });
-
-  },6000);
+  });
 
 });
