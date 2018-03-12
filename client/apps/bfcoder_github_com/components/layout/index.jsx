a55901e3c82c53bc97226f304d93bead58bd1c21
James Carbine <jamescarbine@gmail.com>
2018-03-12 14:44:56 -0600

    WIP

--- client/apps/bfcoder_github_com/components/layout/index.jsx
+++ client/apps/bfcoder_github_com/components/layout/index.jsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
-type Props = {
-  children?: React.Node
-}
+export default class Index extends React.Component {
 
-export default class Index extends React.Component<Props, {}> {
+  static propTypes = {
+    children: PropTypes.node,
+  };
 
   constructor() {
     super();
@@ -14,7 +17,67 @@ export default class Index extends React.Component<Props, {}> {
   render() {
     return (
       <div>
-        {this.props.children || ''}
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/" activeClassName="active">bfcoder</Link>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="/">About</NavItem>
+            <NavItem eventKey={2} href="/projects">Projects</NavItem>
+            <NavDropdown eventKey={3} title="Other" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1} href="http://blog.bfcoder.com">Blog</MenuItem>
+              <MenuItem eventKey={3.2} href="https://github.com/bfcoder">GitHub</MenuItem>
+            </NavDropdown>
+          </Nav>
+        </Navbar>
+
+        <div className="container-fluid">
+
+          {this.props.children || ''}
+
+          <hr />
+
+          <footer>
+            <p>
+              <a href="http://stackoverflow.com/users/1477165/bfcoder">
+                <img
+                  src="http://stackoverflow.com/users/flair/1477165.png?theme=dark"
+                  width="208"
+                  height="58"
+                  alt="profile for bfcoder at Stack Overflow, Q&A for professional and enthusiast programmers"
+                  title="profile for bfcoder at Stack Overflow, Q&A for professional and enthusiast programmers"
+                />
+              </a>
+            </p>
+
+            <p>
+              <a
+                href="https://twitter.com/bfcoder"
+                className="twitter-follow-button"
+                data-show-count="false"
+                data-dnt="true"
+              >
+                Follow @bfcoder
+              </a>
+            </p>
+
+            <p>
+              <a href="http://www.linkedin.com/pub/james-carbine/b/74/628">
+                <img
+                  src="http://www.linkedin.com/img/webpromo/btn_liprofile_blue_80x15.png"
+                  width="80"
+                  height="15"
+                  alt="View James Carbine's profile on LinkedIn"
+                />
+              </a>
+            </p>
+
+            <p>&copy; bfcoder 2015, 2016, 2017</p>
+          </footer>
+
+        </div>
       </div>
     );
   }
--- client/apps/bfcoder_github_com/routes.jsx
+++ client/apps/bfcoder_github_com/routes.jsx
@@ -1,16 +1,18 @@
 // if you use jsx, you have to import React
-import React                          from 'react';
-import { Router, Route, IndexRoute }  from 'react-router';
+import React from 'react';
+import { Router, Route, IndexRoute } from 'react-router';
 
-import appHistory                     from './history';
-import Index                          from './components/layout/index';
-import Home                           from './components/home';
-import NotFound                       from './components/common/not_found';
+import appHistory from './history';
+import Index from './components/layout/index';
+import Home from './components/home';
+import Projects from './components/projects';
+import NotFound from './components/common/not_found';
 
 export default (
   <Router history={appHistory}>
     <Route path="/" component={Index}>
       <IndexRoute component={Home} />
+      <Route path="projects" component={Projects} />
     </Route>
     <Route path="*" component={NotFound} />
   </Router>
