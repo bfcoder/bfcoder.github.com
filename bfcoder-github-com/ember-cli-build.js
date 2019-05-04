dd5747861ffc8c112826b44e9ff612a7a2a66973
James Carbine <jamescarbine@gmail.com>
2019-05-04 13:20:38 -0600

    Fix assets

--- bfcoder-github-com/ember-cli-build.js
+++ bfcoder-github-com/ember-cli-build.js
@@ -7,7 +7,10 @@ module.exports = function(defaults) {
     'ember-bootstrap': {
       'bootstrapVersion': 4,
       'importBootstrapFont': false,
-      'importBootstrapCSS': true
+      'importBootstrapCSS': false
+    },
+    sassOptions: {
+      extension: 'scss'
     }
   });
 
